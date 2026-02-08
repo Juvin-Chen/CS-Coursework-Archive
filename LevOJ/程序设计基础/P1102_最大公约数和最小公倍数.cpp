50268900bea204851d15b14d4b8dc09a186ba493
Juvin <hdcqW@outlook.com>
2026-02-08 22:57:11 +0800

    LevOJ P1102

--- LevOJ/程序设计基础/P1102_最大公约数和最小公倍数.cpp
+++ LevOJ/程序设计基础/P1102_最大公约数和最小公倍数.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 using namespace std;
+
 long long gcd(long long a, long long b) { return b == 0 ? a : gcd(b, a % b); }
 
 long long lcm(long long a, long long b) { return a / gcd(a, b) * b; }
+
 int main() {
     long long a, b;
     cin >> a >> b;
